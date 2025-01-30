c2a712ffc621cda38f45042541a05f85290bb920
Logsun Systems <logsunsystems@gmail.com>
2025-01-30 14:07:06 +0530

    Added Comments

--- Home_Automation/Cloud_Relay.py
+++ Home_Automation/Cloud_Relay.py
@@ -2,19 +2,19 @@ import paho.mqtt.client as mqtt
 import gpiod as GPIO
 import json
 
-THINGSBOARD_HOST = 'demo.thingsboard.io'
-ACCESS_TOKEN = 'ahDTJJKgSCnsqPuUnelS'
+THINGSBOARD_HOST = 'demo.thingsboard.io' # Set the Cloud Hostname
+ACCESS_TOKEN = 'Your_Access_Token'	 # Access token for cloud authentication and access
 
-Relay_1_Pin = 7
-Relay_2_pin = 5
+Relay_1_Pin = 7		# Define the GPIO pin of Relay-1
+Relay_2_pin = 5		# Define the GPIO pin of Relay-2
 
-chip = GPIO.Chip('gpiochip4')
+chip = GPIO.Chip('gpiochip4')	# Select the GPIO chip 
 
-Relay1 = chip.get_line(Relay_1_Pin)
-Relay2 = chip.get_line(Relay_2_pin)
+Relay1 = chip.get_line(Relay_1_Pin)	# Select the GPIO line for Relay-1 pin
+Relay2 = chip.get_line(Relay_2_pin) # Slect the GPIO  line for Relay-2 pin
 
-Relay1.request(consumer = "Relay-1", type = GPIO.LINE_REQ_DIR_OUT)
-Relay2.request(consumer = "Relay-2", type = GPIO.LINE_REQ_DIR_OUT)
+Relay1.request(consumer = "Relay-1", type = GPIO.LINE_REQ_DIR_OUT)	# Set the Relay-1 pin as OUTPUT
+Relay2.request(consumer = "Relay-2", type = GPIO.LINE_REQ_DIR_OUT)	# Set the Relay-2 pin as OUTPUT
 
 # We assume that all GPIOs are LOW
 gpio_state = {7: False, 11: False, 12: False, 13: False, 15: False, 16: False, 18: False, 22: False, 29: False,
@@ -52,17 +52,18 @@ def get_gpio_status():
 
 
 def set_gpio_status(pin, status):
-    if (pin == 26):
-        Relay1.set_value(status)
+    if (pin == 26):	#check the status of Relay-1 Pin
+        Relay1.set_value(status) # Turn Relay ON/OFF accordingly
     
-    if	(pin == 29):
-        Relay2.set_value(status)
+    if	(pin == 29):	#Check the status of Relay-2 Pin
+        Relay2.set_value(status)	#Turn Relay ON/OFF acordingly
     
     # Update GPIOs state
-    gpio_state[pin] = status
+    gpio_state[pin] = status	#update the GPIO status on the cloud
 
 
 
+#Create MQTT Client
 client = mqtt.Client()
 # Register connect callback
 client.on_connect = on_connect
@@ -74,6 +75,7 @@ client.username_pw_set(ACCESS_TOKEN)
 client.connect(THINGSBOARD_HOST, 1883, 60)
 
 try:
-    client.loop_forever()
+    client.loop_forever() #this loop is important for MQTT to work
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    Relay1.release()	# Release the Relay-1 Resource
+    Relay2.release()	# Release the Relay-2 resource
