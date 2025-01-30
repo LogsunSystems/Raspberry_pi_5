dc24127c070d35cc67537864d28fb88e8d29a4db
Logsun Systems <logsunsystems@gmail.com>
2025-01-30 15:01:20 +0530

    Added Comments

--- LED_Pattern/LED_Pattern.py
+++ LED_Pattern/LED_Pattern.py
@@ -1,10 +1,10 @@
 import gpiod
 import time
 LED_PINs=[25,24,23,22,27,18,17,4,26,16,19,13,12,6,5,7]
-led_pin= []
-chip = gpiod.Chip('/dev/gpiochip4')
+chip = gpiod.Chip('/dev/gpiochip4') # select the GPIO chip
 i = 0
 
+# Set the LED pins to Output
 for x in LED_PINs:
     led_pin.append(chip.get_line(x))
     led_pin[i].request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
@@ -14,11 +14,11 @@ try:
    
    while True:
     for l in range (0,len(LED_PINs)):
-        led_pin[l].set_value(1)
-        time.sleep(0.08)
+        led_pin[l].set_value(1) #Turn ON LED
+        time.sleep(0.08)    # Wait for 80 mSec
     
-        led_pin[l].set_value(0)
-        time.sleep(0.05)
+        led_pin[l].set_value(0) #Turn OFF LED
+        time.sleep(0.05)        #Wait for 50 mSec
 finally:
     for l in range (0,len(LED_PINs)):
-        led_pin[l].release()
+        led_pin[l].release()        #release the resources of all the LED pins after use
