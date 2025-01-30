534315a9906a0978dcb04924e6cf215b3de01553
Logsun Systems <logsunsystems@gmail.com>
2025-01-30 19:05:05 +0530

    Added Comments

--- Stepper_Motor/stepper_5V_sw.py
+++ Stepper_Motor/stepper_5V_sw.py
@@ -14,6 +14,7 @@ IP2 = 18
 IP3 = 27
 IP4 = 23
 
+# Define Push Button Pins
 START = 7
 REV   = 5
 STOP  = 13
@@ -27,8 +28,9 @@ Flag_Clockwise = True
 
 
 
-chip = GPIO.Chip('gpiochip4')
+chip = GPIO.Chip('gpiochip4') # Select the GPIO Chip
 
+#Set Motor pins as Output
 EN1_line = chip.get_line(EN1)
 EN1_line.request(consumer="EN1",type = GPIO.LINE_REQ_DIR_OUT)
 
@@ -47,6 +49,7 @@ IP3_line.request(consumer = "IP3", type = GPIO.LINE_REQ_DIR_OUT)
 IP4_line = chip.get_line(IP4)
 IP4_line.request(consumer = "IP4", type = GPIO.LINE_REQ_DIR_OUT)
 
+#set Switch  pins as Input
 STR_line = chip.get_line(START)
 STR_line.request(consumer = "START" , type = GPIO.LINE_REQ_DIR_IN)
 
@@ -63,23 +66,23 @@ STOP_line.request(consumer = "STOP", type = GPIO.LINE_REQ_DIR_IN)
 try:
     while(True):
     
-        if not (STR_line.get_value()):
+        if not (STR_line.get_value()): #Check for Start Button Press
             #print('Start Button Pressed')
-            Flag_Start = True
-            sleep(0.35)
-        if not (REV_line.get_value()):
-            Flag_Clockwise = not Flag_Clockwise
-            sleep(0.35)
-        if not (STOP_line.get_value()):
-            Flag_Start = False
-            sleep(0.35)
+            Flag_Start = True           #Set the Run Flag
+            sleep(0.35)                 #Debounce Delay
+        if not (REV_line.get_value()):  #Check for REV button press
+            Flag_Clockwise = not Flag_Clockwise # Alter the Direction Flag
+            sleep(0.35)                 #Debounce Delay
+        if not (STOP_line.get_value()):    #Check for STOP Button press
+            Flag_Start = False          #Clear the Run Flag
+            sleep(0.35)                 #Debounce Delay
     
-        if (Flag_Start):
+        if (Flag_Start):                #Check for Run Flag
             #print('Run Mode')
-            EN1_line.set_value(1)
-            EN2_line.set_value(1)
+            EN1_line.set_value(1)       #Make EN1 pin High
+            EN2_line.set_value(1)       # Make EN2 Pin High
         
-            if(Flag_Clockwise):
+            if(Flag_Clockwise):         # If Clockwise Flag set, Rotate the motor Clockwise
                 #print('Clock-wise')
                 IP1_line.set_value(1)
                 IP2_line.set_value(0)
@@ -104,7 +107,7 @@ try:
                 IP3_line.set_value(0)
                 IP4_line.set_value(0)
                 sleep(0.01)
-            else:
+            else:                               # Otherwise rotate the motor Anti-clockwise
                 #print('Anit Clock-wise')
                 IP1_line.set_value(1)
                 IP2_line.set_value(0)
@@ -130,7 +133,7 @@ try:
                 IP4_line.set_value(1)
                 sleep(0.01)
 
-        if not (Flag_Start):
+        if not (Flag_Start):            # If Run flag is cleared, Stop the Motor
             #print('Stop')
             EN1_line.set_value(0)
             EN2_line.set_value(0)
@@ -141,6 +144,7 @@ try:
             IP4_line.set_value(0)
         
 finally:
+    #Release all the GPIO Rsources, after use
     EN1_line.release()
     EN2_line.release()
     IP1_line.release()
